ae8fc6c94bb1c803f7716158fb43a7d762d4e86c
JOUANISSONloic <ljouanis@etud.u-pem.fr>
2021-01-10 15:16:06 +0100

    Tableau fini

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <vector>
 
-int main()
+int main(int argc, char** argv)
 {
-    int array[] = { 0, 1, 2, 3 };
-
-    for (int i = 0; i < 4; ++i)
-    {
-        std::cout << array[i] << std::endl;
+    if(argc < 2){
+        std::cerr << "Pas d'argument." <<std::endl;
+        return -1;
+    }
+    int taille = std::stoi(argv[1]);
+    if (taille <= 0){
+        std::cerr << "Taille tableau incorrect." << std::endl;
+        return -1;
+    }
+    std::cout << taille << std::endl;
+    
+    std::vector<int> array;
+    for (int i = 0; i < taille; ++i){
+        array.emplace_back(i);
     }
 
+    for (int value : array){
+        std::cout << value << std::endl;
+    }
+
+    /*
+    int* array = new int[taille] {};
+
+    for (int i = 0; i < taille; ++i){
+        array[i] = i;
+    }
+
+    for (int i = 0; i < taille; ++i){
+    std::cout << array[i] << std::endl;
+    }
+    delete[] array;
+    */
     return 0;
 }
