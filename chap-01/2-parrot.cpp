b20192444fb7df69dc776ab19bfbe9682cf09157
JOUANISSONloic <ljouanis@etud.u-pem.fr>
2021-01-10 14:42:19 +0100

    perroquet parle 10 fois

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,15 @@
-// Implement the program here.
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 10; ++i)
+{
+    string word;
+    cin >> word;
+    cout << "Craow " << word << "!" << endl;
+}
+    return 0;
+}
+
