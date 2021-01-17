c767cde7ed49853cff83c05e936e00f63019466e
JOUANISSONloic <ljouanis@etud.u-pem.fr>
2021-01-17 14:49:44 +0100

    Rectangle fini

--- chap-02/4-rectangle/Rectangle.h
+++ chap-02/4-rectangle/Rectangle.h
@@ -1,22 +1,29 @@
 #pragma once
 
+#include <ostream>
+
 class Rectangle
 {
 public:
-    Rectangle(float length, float width)
-        : _length { length }, _width { width }
-    {}
+    Rectangle();
+    Rectangle(float size);
+    Rectangle(float length, float width);
+//        : _length { length }, _width { width }
+//    {}
 
     float get_length() const { return _length; }
     float get_width() const { return _width; }
 
-    void scale(float ratio)
-    {
-        _length *= ratio;
-        _width *= ratio;
-    }
+    void scale(float ratio);
+//    {
+//        _length *= ratio;
+//        _width *= ratio;
+//    }
+    static float _default_size;
 
 private:
     float _length;
     float _width;
 };
+
+std::ostream& operator<<(std::ostream& stream, const Rectangle& rect);
--- chap-02/4-rectangle/main.cpp
+++ chap-02/4-rectangle/main.cpp
@@ -1,5 +1,4 @@
 #include "Rectangle.h"
-
 #include <iostream>
 
 int main()
@@ -9,6 +8,23 @@ int main()
 
     rect.scale(3);
     std::cout << "{ L: " << rect.get_length() << ", W: " << rect.get_width() << " }" << std::endl;
+
+    Rectangle square(2.5f);
+    std::cout << square << std::endl;
+
+    Rectangle::_default_size = 3.f;
+    Rectangle s1;
+    std::cout << s1 << std::endl;
+    Rectangle s2;
+    std::cout << s2 << std::endl;
+
+    Rectangle::_default_size = 5.f;
+    Rectangle s3;
+    std::cout << s3 << std::endl;
+    Rectangle s4;
+    std::cout << s4 << std::endl;
+    Rectangle s5;
+    std::cout << s5 << std::endl;
 
     return 0;
 }
